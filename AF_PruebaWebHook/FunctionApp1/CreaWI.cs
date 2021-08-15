7033983e54a313954257b49b480eba156b9e3453
Sebastian Ramos <sebastian.ramos.pina@gmail.com>
2021-08-15 14:47:13 -0400

    vito

--- AF_PruebaWebHook/FunctionApp1/CreaWI.cs
+++ AF_PruebaWebHook/FunctionApp1/CreaWI.cs
@@ -10,29 +10,9 @@ using Newtonsoft.Json;
 
 namespace FunctionApp1
 {
-    public static class Function1
+    public static class CreaWI
     {
-        [FunctionName("Function1")]
-        public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-            ILogger log)
-        {
-            log.LogInformation("C# HTTP trigger function processed a request.");
-
-            string name = req.Query["name"];
-
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
-
-            return new OkObjectResult(responseMessage);
-        }
-
-        [FunctionName("Function2")]
+        [FunctionName("CreaWI")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
